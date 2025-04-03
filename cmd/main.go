b944055347325c2fe9a9fa8372d3c084be13f185
Anang Firmansyah <anangfirmansyahp5@gmail.com>
2025-04-03 12:52:04 +0800

    init db config

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,27 @@ import (
 	"log"
 
 	"github.com/anangfirmansyahputra/ecom/cmd/api"
+	"github.com/anangfirmansyahputra/ecom/config"
+	"github.com/anangfirmansyahputra/ecom/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
